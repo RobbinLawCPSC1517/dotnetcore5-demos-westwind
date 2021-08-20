a16b3e3fea8ef19e5ba8e8312d86a0bc739bac6d
robbinl <robbinl@telusplanet.net>
2021-08-20 14:29:23 -0600

    demos aa_console1 bb_console2

---
+++ Test/bb_console2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace bb_console2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Ex01();
+        }
+
+        private void Ex01()
+        {
+            try
+            {
+                Console.WriteLine("Ex01 Program started");
+                //throw new Exception($"Hey Man an exception was thrown"); 
+                Console.WriteLine("Ex01 Program ended");
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine($"Exception in Ex01: {ex.Message}");
+                //throw;
+            }
+            finally
+            {
+                Console.WriteLine("The finally runs exception or not");
+            }
+            
+        }
+    }
+}
---
+++ Test/cc_console3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cc_console3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
