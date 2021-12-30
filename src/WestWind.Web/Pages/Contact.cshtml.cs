8ef19489fbc5b484294d8d8bdd810f6630864feb
robbinl <robbinl@telusplanet.net>
2021-12-30 13:21:44 -0700

    a

--- src/WestWind.Web/Pages/Contact.cshtml.cs
+++ src/WestWind.Web/Pages/Contact.cshtml.cs
@@ -12,22 +12,29 @@ namespace MyApp.Namespace
     public class ContactModel : PageModel
     {
         // Allows for one-way binding of data
-        [BindProperty]
         public string Text1{get;set;}
-        
+        // Allows for two-way binding of data
+        [BindProperty] 
+        public string Text2{get;set;}
         // Allows for two-way binding of data
         [BindProperty] 
         public string Text3{get;set;}
 
-        [BindProperty] 
         public int Number1{get;set;}
-
+        [BindProperty] 
+        public int Number2{get;set;}
+        [BindProperty] 
+        public int Number3{get;set;}
+        [BindProperty]
         public string Email{get;set;}
         public List<string> SelectListOfSubjects{get;set;}
+        [BindProperty]
         public int SelectedSubjectId {get;set;}
+        [BindProperty]
         public string MessageBody{get;set;}
+        [BindProperty]
         public bool ActiveMember{get;set;}
-
+        [BindProperty]
         public string ButtonPressed {get; set;}
         public string SuccessMessage {get; set;}
         public string ErrorMessage {get; set;}
@@ -50,36 +57,40 @@ namespace MyApp.Namespace
             }
         }
 
-        public IActionResult OnPost(string buttonPressed, string text1, string email, string selectedSubjectId, string messageBody, string activeMember)
+        public IActionResult OnPost(string buttonPressed, string text1, string number1, string email, string selectedSubjectId, string messageBody, string activeMember)
         {
             try
             {
                 Console.WriteLine($"ContactModel: OnPost");
-                ButtonPressed = buttonPressed;
-                if(ButtonPressed == "Clear")
-                    return RedirectToPage("Contact");
-                PopulateSelectLists();
-                Text1 = text1;
-                Email = email;
-                if(!string.IsNullOrEmpty(selectedSubjectId))
-                    SelectedSubjectId = int.Parse(selectedSubjectId);
-                MessageBody = messageBody;
-                Console.WriteLine($"Actual form checkbox={activeMember}");
-                if(string.IsNullOrEmpty(activeMember))
+                //ButtonPressed = buttonPressed;
+                if(ButtonPressed == "Submit")
                 {
-                    ActiveMember = false;
-                }
-                else
+                    PopulateSelectLists();
+                    Text1 = text1;
+                    //Email = email;
+                    // if(!string.IsNullOrEmpty(selectedSubjectId))
+                    //     SelectedSubjectId = int.Parse(selectedSubjectId);
+                    // MessageBody = messageBody;
+                    Console.WriteLine($"Actual form checkbox={activeMember}");
+                    if(string.IsNullOrEmpty(activeMember))
+                    {
+                        ActiveMember = false;
+                    }
+                    else
+                    {
+                        ActiveMember = true;
+                    }
+                    if (SelectedSubjectId == 0)
+                        errors.Add(new Exception("Subject"));
+                    if (string.IsNullOrEmpty(Text1))
+                        errors.Add(new Exception("Text1"));
+                    if (errors.Count() > 0)
+                        throw new AggregateException("Missing Data: ", errors);
+                    SuccessMessage = $"T1: {Text1} T2: {Text2} T3: {Text3} N1: {Number1} N2: {Number2} Email: {Email} Subject: {SelectListOfSubjects[SelectedSubjectId]} Text: {MessageBody} ActiveMember: {ActiveMember}";
+                } else if(ButtonPressed == "Clear")
                 {
-                    ActiveMember = true;
+
                 }
-                if (SelectedSubjectId == 0)
-                    errors.Add(new Exception("Select a subject"));
-                if (string.IsNullOrEmpty(Text1))
-                    errors.Add(new Exception("Text1 cannot be empty"));
-                if (errors.Count() > 0)
-                    throw new AggregateException("Aggregate Exception Message", errors);
-                SuccessMessage = $"T1: {Text1} T3: {Text3} N1: {Number1} Email: {Email} Subject: {SelectListOfSubjects[SelectedSubjectId]} Text: {MessageBody} ActiveMember: {ActiveMember}";
                 // Return the page but preserve any user inputs
                 return Page();
             }
@@ -106,10 +117,10 @@ namespace MyApp.Namespace
 
         public void GetInnerException(AggregateException e)
         {
-            ErrorMessage = $"Errors: ";
+            ErrorMessage = e.Message;
             foreach (Exception innerException in e.InnerExceptions)
                 {
-                    ErrorMessage += innerException.Message;
+                    //ErrorMessage += innerException.Message;
                     Console.WriteLine(innerException.Message);
                 }
             // Start with the assumption that the given exception is the root of the problem
